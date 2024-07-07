98e0e42a97e2c61be8bb2a827b85b5ed2ebe2ee1
Yuki Shishido <yuki.shishido@optim.co.jp>
2024-07-07 23:59:03 +0900

    WIp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import './index.css';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,22 +9,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='flex flex-col'>
+        <button className = 'bg-blue-100' onClick={() => setCount((count) => count + 1)}>
+          up
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button className='bg-red-100 hover:bg-red-300' onClick={() => setCount((count)=> count -1 )}>
+         down
+         </button>
+         <p>
+          {count}
+         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
