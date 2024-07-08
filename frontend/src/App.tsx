addc7617f7f826b349f7e15b87b2154f0da9ee2c
Yuki Shishido <yuki.shishido@optim.co.jp>
2024-07-09 01:06:49 +0900

    フォルダのパスが誤っていたので修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './index.css';
 import { useState } from 'react'
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dir, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
