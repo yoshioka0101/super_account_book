4458332c20287ab8c3d74cc808ab25224581d25a
Yuki Shishido <yuki.shishido@optim.co.jp>
2024-07-09 22:39:36 +0900

    feat: バックエンドにtotal-spentエンドポイントとサンプルデータを追加
    - total-spentエンドポイントを追加し、合計支出を計算
    - fakeExpenses配列にサンプル支出データを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
 import './index.css';
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react'
+// import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [totalSpent, setTotalSpent] = useState(0)
+
+  useEffect(() => {
+    fetch("/api/expenses/total-spent")
+  }, [])
 
   return (
-    <>
-      <div className='flex flex-col bg-background max-w-md m-auto gap-y-5'>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          up
-        </Button>
-        <Button onClick={() => setCount((count)=> count -1 )}>
-         down
-         </Button>
-         <p>
-          {count}
-         </p>
-      </div>
-    </>
+    <Card className="w-[350px] m-auto">
+      <CardHeader>
+        <CardTitle>Total Spent</CardTitle>
+        <CardDescription>the total amount you are spent</CardDescription>
+      </CardHeader>
+      <CardContent>{totalSpent}</CardContent>
+      <CardFooter>
+      </CardFooter>
+    </Card>
   )
 }
 
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import app from './app'
 
+const port = process.env.PORT || 3000; // ここでポートを指定
+
 Bun.serve({
-  fetch: app.fetch
+  fetch: app.fetch,
+  port: port
 })
-console.log("server runnning!");
