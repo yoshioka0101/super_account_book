2ec9663d8364933b0be26d2d9ab58f4037567e84
Yuki Shishido <yuki.shishido@optim.co.jp>
2024-07-15 13:51:55 +0900

    serveStaticを追加

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { expensesRoutes } from './server/routes/expenses'
+import { serveStatic } from 'hono/bun'
 
 
 const app = new Hono()
@@ -12,5 +13,8 @@ app.get("/test", c => {
 } )
 
 app.route("/api/expenses", expensesRoutes)
+
+app.use('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app // for Cloudflare Workers or Bun
