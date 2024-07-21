677a90faf1311d3e9eba89719cacd2d657493e65
Yuki Shishido <yuki.shishido@optim.co.jp>
2024-07-21 20:57:27 +0900

    feat: Expenses コンポーネントにデータ取得機能を追加
    - useQueryフックを使用してAPIからデータを取得するgetAllExpensesを追加
    - Expensesコンポーネント内でデータの取得状態に応じた表示を実装
    - データが取得されている場合、JSONを表示

--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -1,9 +1,27 @@
+import React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { api } from '@/lib/api'
+import { useQuery } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/expenses')({
   component: Expenses,
 })
 
+async function getAllExpenses() {
+  const result = await api.expenses["total-spent"].$get()
+  const data = await result.json()
+  return data
+}
+
 function Expenses() {
-  return <div className="p-2">Hello from expenses!</div>
+  const { isPending, error, data } = useQuery({
+    queryKey: ['get-all-expenses'],
+    queryFn: getAllExpenses
+  })
+
+  return (
+    <div className="p-2">
+      {isPending ? "..." : JSON.stringify(data, null, 2)}
+    </div>
+  )
 }
