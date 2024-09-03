65d3f01d0cd62e97e0e3045bf4ca392022729889
Yuki Shishido <yuki.shishido@optim.co.jp>
2024-09-03 20:19:53 +0900

    feat:prismaをインストール

---
+++ prisma/migrations/20240903111532_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "date" DATETIME NOT NULL,
+    "title" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "tag" TEXT NOT NULL
+);
