2e6ba744f26afa9b266bd94ab301666c13ee124c
yuki.shishido <yuki.shishido@optim.co.jp>
2024-10-03 22:51:44 +0900

    feat:postgresql導入

--- server/prismaClient.ts
+++ server/prismaClient.ts
@@ -1,5 +1,9 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`
 
-export default prisma;
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
